006283af14675ecb6f494ae6adf32054d1406f2b
FairyEver <1711467488@qq.com>
2020-07-07 19:12:42 +0800

    feat: :sparkles: 优化搜索时侧边栏显示

--- src/components/app-search-filter/component.vue
+++ src/components/app-search-filter/component.vue
@@ -8,47 +8,51 @@
 </style>
 
 <template>
-  <v-card class="app-search-filter py-2 pr-2" color="primary" dark flat>
+  <v-card class="app-search-filter pa-1" color="primary" flat dark>
     <app-scroll class="app-search-filter__scroll">
-      <v-card-title>
-        <v-icon left>mdi-star-circle</v-icon>
-        <span class="font-weight-light">分类</span>
-      </v-card-title>
-      <v-card-text>
-        <v-radio-group v-model="collection">
-          <v-radio
-            v-for="option in dict.collection"
-            :key="option.name"
-            :label="option.label"
-            :value="option.name"/>
-        </v-radio-group>
-      </v-card-text>
-      <v-card-title>
-        <v-icon left>mdi-invert-colors</v-icon>
-        <span class="font-weight-light">填充</span>
-      </v-card-title>
-      <v-card-text>
-        <v-radio-group v-model="fills">
-          <v-radio
-            v-for="option in dict.fills"
-            :key="option.name"
-            :label="option.label"
-            :value="option.name"/>
-        </v-radio-group>
-      </v-card-text>
-      <v-card-title>
-        <v-icon left>mdi-palette</v-icon>
-        <span class="font-weight-light">风格</span>
-      </v-card-title>
-      <v-card-text>
-        <v-radio-group v-model="style">
-          <v-radio
-            v-for="option in dict.style"
-            :key="option.name"
-            :label="option.label"
-            :value="option.name"/>
-        </v-radio-group>
-      </v-card-text>
+      <v-expansion-panels flat multiple accordion tile hover>
+        <v-expansion-panel>
+          <v-expansion-panel-header color="primary">
+            <v-icon class="flex-grow-0" left>mdi-star-circle</v-icon>
+            <span class="font-weight-bold">分类</span>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content color="primary">
+            <v-radio-group v-model="collection" hide-details>
+              <v-radio v-for="option in dict.collection" :key="option.name" :label="option.label" :value="option.name"/>
+            </v-radio-group>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header color="primary">
+            <v-icon class="flex-grow-0" left>mdi-invert-colors</v-icon>
+            <span class="font-weight-bold">填充</span>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content color="primary">
+            <v-radio-group v-model="fills" hide-details>
+              <v-radio
+                v-for="option in dict.fills"
+                :key="option.name"
+                :label="option.label"
+                :value="option.name"/>
+            </v-radio-group>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header color="primary">
+            <v-icon class="flex-grow-0" left>mdi-palette</v-icon>
+            <span class="font-weight-bold">风格</span>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content color="primary">
+            <v-radio-group v-model="style" hide-details>
+              <v-radio
+                v-for="option in dict.style"
+                :key="option.name"
+                :label="option.label"
+                :value="option.name"/>
+            </v-radio-group>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </app-scroll>
   </v-card>
 </template>
