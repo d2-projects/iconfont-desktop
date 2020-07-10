253b285e6f548afa8a2963b39cfc4a8eb5e67e90
FairyEver <1711467488@qq.com>
2020-07-10 14:48:47 +0800

    feat: :sparkles: automatic-register folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-const files = require.context('./', true, /component\.vue$/)
+const files = require.context('./automatic-register/', true, /component\.vue$/)
 
 files.keys().forEach(key => {
   Vue.component(files(key).default.name, files(key).default)
