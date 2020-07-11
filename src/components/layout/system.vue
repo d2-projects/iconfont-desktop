7cd9b8b853aab0e86eb0836c25e9fb4d8b97588f
FairyEver <1711467488@qq.com>
2020-07-11 17:06:07 +0800

    feat: :sparkles: 顶部进度条

--- src/components/layout/system.vue
+++ src/components/layout/system.vue
@@ -7,25 +7,33 @@
     app
     dark
     window>
-    <v-icon left>
-      {{ icon }}
-    </v-icon>
-    <span>
-      {{ title }}
-    </span>
-    <v-spacer/>
-    <v-icon v-if="!isFullScreen" @click="minimize">
-      mdi-minus
-    </v-icon>
-    <v-icon v-if="!isFullScreen" @click="toggleMaximize">
-      {{ isMaximized ? 'mdi-window-restore' : 'mdi-window-maximize' }}
-    </v-icon>
-    <v-icon @click="toggleFullScreen">
-      {{ isFullScreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen' }}
-    </v-icon>
-    <v-icon @click="close">
-      mdi-close
-    </v-icon>
+    <v-row no-gutters>
+      <v-col cols="5">
+        <v-icon left>{{ icon }}</v-icon>
+        <span>{{ title }}</span>
+      </v-col>
+      <v-col cols="2" flex="main:center cross:center">
+        <v-row justify="center" no-gutters>
+          <v-col xs="10" sm="8" md="6" lg="5" xl="4">
+            <v-progress-linear color="white" rounded indeterminate/>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="5" flex="main:right">
+        <v-icon v-if="!isFullScreen" @click="minimize">
+          mdi-minus
+        </v-icon>
+        <v-icon v-if="!isFullScreen" @click="toggleMaximize">
+          {{ isMaximized ? 'mdi-window-restore' : 'mdi-window-maximize' }}
+        </v-icon>
+        <v-icon @click="toggleFullScreen">
+          {{ isFullScreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen' }}
+        </v-icon>
+        <v-icon @click="close">
+          mdi-close
+        </v-icon>
+      </v-col>
+    </v-row>
   </v-system-bar>
 </template>
 
