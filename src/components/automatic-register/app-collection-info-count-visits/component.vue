d2786d7f84c28bc951cce90c263c6deaeeb8e443
FairyEver <1711467488@qq.com>
2020-07-16 15:30:41 +0800

    feat: :sparkles: 增加访问人数提示

--- src/components/automatic-register/app-collection-info-count-visits/component.vue
+++ src/components/automatic-register/app-collection-info-count-visits/component.vue
@@ -1,8 +1,13 @@
 <template>
-  <span class="d-flex align-center flex-shrink-0">
-    <v-icon size="22" color="grey" left>mdi-eye-outline</v-icon>
-    <span class="font-weight-medium">{{ value | numberFormat }}</span>
-  </span>
+  <v-tooltip top open-delay="500">
+    <template v-slot:activator="{ on, attrs }">
+      <span class="d-flex align-center flex-shrink-0" v-on="on" v-bind="attrs">
+        <v-icon size="22" color="grey" left>mdi-eye-outline</v-icon>
+        <span class="font-weight-medium">{{ value | numberFormat }}</span>
+      </span>
+    </template>
+    <span>{{ tooltipText }}</span>
+  </v-tooltip>
 </template>
 
 <script>
@@ -12,6 +17,11 @@ export default {
     value: {
       type: Number,
       default: 0
+    }
+  },
+  computed: {
+    tooltipText () {
+      return this.value < 1000 ? '访问人数' : `${this.value} 人看过`
     }
   }
 }
