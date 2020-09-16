2937c86c3d27939e0060bfab6d021ed6d2055a0e
FairyEver <1711467488@qq.com>
2020-09-16 15:28:14 +0800

    feat: app-list-item-content

--- src/components/app-list-item-content/component.vue
+++ src/components/app-list-item-content/component.vue
@@ -1,11 +1,48 @@
+<style lang="scss">
+@include b(list-item-content) {
+  @extend .absolute-all;
+  @include e(layer) {
+    @extend .absolute-all;
+  }
+  @include e(selected) {
+    box-shadow: map-get($blue, 'darken-2') 0px 0px 0px 2px inset;
+    background-color: map-get($blue, 'lighten-5');
+  }
+}
+</style>
+
 <template>
-  <app-square class="white rounded" center>
-    <slot/>
+  <app-square
+    class="
+      app-list-item-content
+      white
+      rounded
+      is-pointer
+    ">
+    <!-- 用来显示边框 -->
+    <div
+      v-show="selected"
+      class="
+        app-list-item-content__layer
+        app-list-item-content__selected
+        rounded
+      "/>
+    <!-- 用来显示内容 -->
+    <div
+      class="app-list-item-content__layer"
+      flex="main:center cross:center">
+      <slot/>
+    </div>
   </app-square>
 </template>
 
 <script>
 export default {
-  name: 'app-list-item-content'
+  name: 'app-list-item-content',
+  props: {
+    selected: {
+      type: Boolean
+    }
+  }
 }
 </script>
