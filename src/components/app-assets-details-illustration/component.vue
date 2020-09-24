71d35d139ef82d820a71694824ca793589efea62
FairyEver <1711467488@qq.com>
2020-09-24 21:33:49 +0800

    feat: app-assets-details-illustration file

---
+++ src/components/app-assets-details-illustration/component.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-dialog
+    :value="true"
+    width="300"
+    overlay-color="rgba(0, 0, 0, 0)"
+    persistent>
+    <v-card>
+      app-assets-details-illustration
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'app-assets-details-illustration'
+}
+</script>
